263da8d4ba482128573ad3c7f86d65c5af95583b
palaks6666 <63353778+palaks6666@users.noreply.github.com>
2020-07-16 03:00:35 +0530

    Update Q7 - Binary Search.java

--- Q7 - Binary Search.java
+++ Q7 - Binary Search.java
@@ -21,6 +21,10 @@ class Main{
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+         arr[i]=in.nextInt();
+        }
+        Arrays.sort(arr);
         int key = 30;  
         int last=arr.length-1;  
         binarySearch(arr,0,last,key);     
